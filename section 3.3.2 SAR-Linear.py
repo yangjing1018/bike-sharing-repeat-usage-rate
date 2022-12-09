5c7cd0023d39769c3dd27d6ee387505bc68f61ca
yangjing1018 <120155117+yangjing1018@users.noreply.github.com>
2022-12-09 15:36:12 +0800

    Add files via upload

---
+++ section 3.3.2 SAR-Linear.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+import geohash
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def getGeo(x,y):
+    return geohash.encode(y,x,precision=7)
+
+if __name__ == "__main__":
+    #generate variables
+    df = pd.read_csv("/Users/yangjing/Desktop/dissertation/mobike_shanghai_sample_updated.csv")
+    df['start_time'] = pd.to_datetime(df['start_time'])
+    df['end_time'] = pd.to_datetime(df['end_time'])
+    dic_micro = {"hour":[],"user_unlock":[],"followup_orders_unlock_24h":[],"followup_orders_unlock_24to72r":[],
+                  "followup_orders_unlock_72to168h":[],"user_lock":[],"followup_orders_lock_24h":[],
+                  "followup_orders_lock_24to72h":[],"followup_orders_lock_72to168h":[],
+                   "mean_temp":[],"air":[],"GDP":[],"spatial":[]}
+
+    df_macro = pd.read_excel("/Users/yangjing/Desktop/dissertation/macro.xlsx")
+    df_macro.columns = ['date','is_weekday','max_temp','min_temp','mean_temp','is_rain',
+                       'air','wind','house_price','population','GDP']
+
+    df_sdm = pd.read_excel("/Users/yangjing/Desktop/dissertation/SAR.xlsx")
+    # df_macro = df_macro[['max_temp','min_temp','air','wind','house_price','population','GDP']]
+
+
+    for day in range(22,29):
+        for i in range(0,24):
+            lock_average = len(df[(df["end_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])/7
+            lock_1 = len(df[(df["end_time"] >= datetime(2016, 8, day-1,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])
+            lock_3 = len(df[(df["end_time"] >= datetime(2016, 8, day-3,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])
+            lock_7 = len(df[(df["end_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])
+
+            unlock_average = len(df[(df["start_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])/7
+            unlock_1 = len(df[(df["start_time"] >= datetime(2016, 8, day-1,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])
+            unlock_3 = len(df[(df["start_time"] >= datetime(2016, 8, day-3,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])
+            unlock_7 = len(df[(df["start_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])
+
+            dic_micro["hour"].append(i)
+            dic_micro["user_unlock"].append(unlock_average)
+            dic_micro["followup_orders_unlock_24h"].append(unlock_1)
+            dic_micro["followup_orders_unlock_24to72r"].append(unlock_3)
+            dic_micro["followup_orders_unlock_72to168h"].append(unlock_7)
+            dic_micro["user_lock"].append(lock_average)
+            dic_micro["followup_orders_lock_24h"].append(lock_1)
+            dic_micro["followup_orders_lock_24to72h"].append(lock_3)
+            dic_micro["followup_orders_lock_72to168h"].append(lock_7)
+            dic_micro["mean_temp"].append(df_macro["mean_temp"][0])
+            dic_micro["air"].append(df_macro["air"][0])
+            dic_micro["GDP"].append(df_macro["GDP"][0])
+
+            df_temp = df_sdm[df_sdm["date"]==day]
+            df_temp = df_temp[df_temp["hour"]==i]
+            dic_micro["spatial"].append(df_temp["spatial"].mean())
+    x = pd.DataFrame(dic_micro)#complete
+
+    #generate repeat usage rate
+    df_zi = pd.read_csv("result_repeat usage rate.csv",encoding="gb18030")
+    y = pd.DataFrame()
+    for day in range(22,29):
+        df_day = df_zi[df_zi["date"]==day]
+        df_day = df_day.groupby('hour').mean()
+        y = pd.concat([y,df_day["48h"]])
+    y = y.reset_index(drop=True)
+    import statsmodels.api as sm
+    print("---------------------------------display variables---------------------------------\n")
+    print(x)
+    print("\n"*3)
+    print("---------------------------------display variables---------------------------------\n")
+    print(y)
+    model = sm.OLS(y, x.astype(float))  # generate model
+    result = model.fit()  # fit model
+    print("\n"*3)
+    print("---------------------------------Summary of linear model parameters---------------------------------\n")
+    print(result.summary())
+    formula = ""
+    # print(int(result.params[0]))
+    for o in range(13):
+        # parameter = str(round(result.params[o], 10))
+        parameter = str(result.params[o])
+        if result.params[o] > 0:
+            formula = formula + "+" + parameter + " * " + x.columns[o]
+        if result.params[o] < 0:
+            formula = formula + parameter + " * " + x.columns[o]
+    print("\n"*3)
+    print("---------------------------------repeate usage rate---------------------------------\n")
+    print("repeat usage rate=",formula[1:])
+    print("---------------------------------display evaluation indicators---------------------------------\n")
+    yp = result.predict(x)
+    resid = y-yp
+    rss = np.sum(resid**2)
+    MSE = rss/(result.nobs)
+    RMSE = np.sum(MSE**2)
+    print("RMSE:",RMSE)
+
+    APE = 0
+    y = y.values
+    for q in range(len(y)):
+        APE = abs((yp[q]-y[q])/y[q])
+    MAPE = APE / (result.nobs)
+    print("MAPE:",MAPE[0])
---
+++ section 3.3.3 SAR-XGBoost.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import geohash
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import xgboost as xgb
+from xgboost import plot_importance
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_boston
+
+def regularit(df):
+    newDataFrame = pd.DataFrame(index=df.index)
+    columns = df.columns.tolist()
+    for c in columns:
+        d = df[c]
+        MAX = d.max()
+        MIN = d.min()
+        newDataFrame[c] = ((d - MIN) / (MAX - MIN)).tolist()
+    return newDataFrame
+
+
+def getGeo(x,y):
+    return geohash.encode(y,x,precision=7)
+
+if __name__ == "__main__":
+    #generate variables
+    df = pd.read_csv("/Users/yangjing/Desktop/dissertation/mobike_shanghai_sample_updated.csv")
+    df['start_time'] = pd.to_datetime(df['start_time'])
+    df['end_time'] = pd.to_datetime(df['end_time'])
+    dic_micro = {"hour":[],"user_unlock":[],"followup_orders_unlock_24h":[],"followup_orders_unlock_24to72r":[],
+                  "followup_orders_unlock_72to168h":[],"user_lock":[],"followup_orders_lock_24h":[],
+                  "followup_orders_lock_24to72h":[],"followup_orders_lock_72to168h":[],
+                   "mean_temp":[],"air":[],"GDP":[],"spatial":[]}
+
+    df_macro = pd.read_excel("/Users/yangjing/Desktop/dissertation/macro data.xlsx")
+    df_macro.columns = ['date','is_weekday','max_temp','min_temp','mean_temp','is_rain',
+                       'air','wind','house_price','population','GDP']
+
+    df_sdm = pd.read_excel("/Users/yangjing/Desktop/dissertation/SAR.xlsx")
+    # df_macro = df_macro[['max_temp','min_temp','air','wind','house_price','population','GDP']]
+
+
+    for day in range(22,29):
+        for i in range(0,24):
+            lock_average = len(df[(df["end_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])/7
+            lock_1 = len(df[(df["end_time"] >= datetime(2016, 8, day-1,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])
+            lock_3 = len(df[(df["end_time"] >= datetime(2016, 8, day-3,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])
+            lock_7 = len(df[(df["end_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["end_time"] < datetime(2016, 8, day,i,0,0))])
+
+            unlock_average = len(df[(df["start_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])/7
+            unlock_1 = len(df[(df["start_time"] >= datetime(2016, 8, day-1,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])
+            unlock_3 = len(df[(df["start_time"] >= datetime(2016, 8, day-3,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])
+            unlock_7 = len(df[(df["start_time"] >= datetime(2016, 8, day-7,i,0,0)) & (df["start_time"] < datetime(2016, 8, day,i,0,0))])
+
+            dic_micro["hour"].append(i)
+            dic_micro["user_unlock"].append(unlock_average)
+            dic_micro["followup_orders_unlock_24h"].append(unlock_1)
+            dic_micro["followup_orders_unlock_24to72r"].append(unlock_3)
+            dic_micro["followup_orders_unlock_72to168h"].append(unlock_7)
+            dic_micro["user_lock"].append(lock_average)
+            dic_micro["followup_orders_lock_24h"].append(lock_1)
+            dic_micro["followup_orders_lock_24to72h"].append(lock_3)
+            dic_micro["followup_orders_lock_72to168h"].append(lock_7)
+            dic_micro["mean_temp"].append(df_macro["mean_temp"][0])
+            dic_micro["air"].append(df_macro["air"][0])
+            dic_micro["GDP"].append(df_macro["GDP"][0])
+
+            df_temp = df_sdm[df_sdm["日期"]==day]
+            df_temp = df_temp[df_temp["小时"]==i]
+            dic_micro["spatial"].append(df_temp["spatial"].mean())
+    x = pd.DataFrame(dic_micro)#complete 
+
+    #generate repeat usage
+    df_zi = pd.read_csv("result_repeat usage rate.csv",encoding="gb18030")
+    y = pd.DataFrame()
+    for day in range(22,29):
+        df_day = df_zi[df_zi["date"]==day]
+        df_day = df_day.groupby('hour').mean()
+        y = pd.concat([y,df_day["48h"]])
+    y = y.reset_index(drop=True)
+    print("---------------------------------display variables---------------------------------\n")
+    name = x.columns
+    x = x.astype(float)
+    # x = regularit(x)
+    x = x*100
+    print("\n"*3)
+    print("---------------------------------display variables---------------------------------\n")
+    # y = y*10000000000*-1
+    y = y.values
+    y = y.astype(np.float)
+    # y = pd.DataFrame(np.random.randint(0,6,size=(23, 1)))
+    print(y)
+    print("\n"*3)
+    print("---------------------------------Summary of XGBoost model parameters ---------------------------------\n")
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=15)
+    from xgboost import XGBRegressor
+    xgb = XGBRegressor(n_estimators=500,
+                       max_depth=5,
+                       learning_rate=0.1,
+                       gamma=0,
+                       min_child_weight=1,
+                       reg_alpha=0,
+                       reg_lambda=1
+                       )
+    xgb.fit(X_train, y_train)
+    # feature = np.array([0.87751926,0.05,0.05781318,0.05,0.05,0.05,
+    #                                      0.01466756,0.05,0.05,0.05,0.05,0.05,])
+    print(xgb.feature_importances_)
+    # plt.barh(name, feature)
+    plt.barh(name, xgb.feature_importances_)
+    plt.tight_layout()
+    plt.savefig("/Users/yangjing/Desktop/dissertation/SAR-XGBoost", dpi=600)
+    plt.show()
+    print("---------------------------------display of evaluation indicators--------------------------------\n")
+    from sklearn.metrics import mean_squared_error,r2_score
+    y_pred = xgb.predict(X_test)
+    RMSE = np.sqrt(mean_squared_error(y_pred, y_test))
+
+    APE = []
+    for t in range(len(y_test)):
+        per_err = (y_test[t] - y_pred[t]) / y_test[t]
+        per_err = abs(per_err)
+        APE.append(per_err)
+    MAPE = sum(APE) / len(APE)
+    print("R-squared=",r2_score(y_test,y_pred))
+    print("RMSE=", RMSE)
+    print("MAPE=", MAPE[0])
